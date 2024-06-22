76e3f5bf644c9277b30e7a694fa4ba8fcbe37345
Abdul Afroz <abdulafroz59963@gmail.com>
2024-06-22 18:18:15 +0530

    added FarmerQuery and Volunteer model

--- IFTR/accounts/models.py
+++ IFTR/accounts/models.py
@@ -12,6 +12,31 @@ class Farmer(models.Model):
     def __str__(self):
         return f'{self.f_name}'
 
+class FarmerQuery(models.Model):
+    fq_id= models.ForeignKey(Farmer, on_delete=models.CASCADE,related_name="farmer_query")
+    fq_area_ploughed = models.CharField(max_length=100)
+    fq_season = models.CharField(max_length=100)
+    fq_crop_grown = models.CharField(max_length=100)
+    fq_seeds_used = models.CharField(max_length=1000)
+    fq_date_of_seed_sown = models.DateField()
+    fq_transplanting = models.CharField(max_length=100)
+    fq_irrigation = models.CharField(max_length=100)
+    fq_fertilizers_used = models.CharField(max_length=1000)
+    fq_date_of_harvest=models.DateField()
+    fq_yield = models.IntegerField(max_length=1000)
+
+    def __str__(self):
+        return f'{self.fq_id.f_name}'
+
+class Volunteer(models.Model):
+    v_id= models.OneToOneField(User,primary_key=True,on_delete=models.CASCADE)
+    v_name=  models.CharField(max_length=100)
+    v_aadhar_number= models.BigIntegerField()
+    v_contact_number= models.BigIntegerField()
+    v_location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.v_name}'
 
 
 
